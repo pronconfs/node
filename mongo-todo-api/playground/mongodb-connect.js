576a919f7b76227e0b67a473d69aa80c5c658466
Paulo <proncon@gmail.com>
2018-03-24 17:49:43 +0000

    New module: Todo API II

--- mongo-todo-api/playground/mongodb-connect.js
+++ mongo-todo-api/playground/mongodb-connect.js
@@ -6,5 +6,28 @@ MongoClient.connect('mongodb://msr:msr@ds123029.mlab.com:23029/todo-api', functi
     }
     console.log('Connected to mongoDB');
 
+    // db.collection('Todos').insertOne({
+    //     text: 'something to do',
+    //     completed: false
+    // }, function (err,res) {
+    //     if (err) {
+    //         return console.log('unable to insert Todo ', err);
+    //     }
+
+    //     console.log(JSON.stringify(res.ops, undefined, 2));
+    // });
+
+    db.collection('Users').insertOne({
+        name: 'paulo',
+        age: 20,
+        Location: 'PT'
+    }, function (err,res) {
+        if (err) {
+            return console.log('unable to insert Users ', err);
+        }
+
+        console.log(JSON.stringify(res.ops, undefined, 2));
+    });
+
     db.close();
 });
