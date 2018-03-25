8594528abee6b65e285c23347409cf0ff56c0eeb
paulo_roncon <proncon@gmail.com>
2018-03-25 17:58:24 +0100

    all merged

--- mongo-todo-api/playground/mongodb-connect.js
+++ mongo-todo-api/playground/mongodb-connect.js
@@ -1,33 +1,41 @@
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://msr:msr@ds123029.mlab.com:23029/todo-api', function(err,db) {
-    if (err) {
-        return console.log('Unable to connect to mongoDB.');
-    }
-    console.log('Connected to mongoDB');
+MongoClient.connect('mongodb://msr:msr@ds123029.mlab.com:23029/todo-api', function (err, db) {
+	if (err) {
+		return console.log('Unable to connect to mongoDB.');
+	}
+	console.log('Connected to mongoDB');
 
-    // db.collection('Todos').insertOne({
-    //     text: 'something to do',
-    //     completed: false
-    // }, function (err,res) {
-    //     if (err) {
-    //         return console.log('unable to insert Todo ', err);
-    //     }
+	// db.collection('Todos').insertOne({
+	//     text: 'something to do',
+	//     completed: false
+	// }, function (err,res) {
+	//     if (err) {
+	//         return console.log('unable to insert Todo ', err);
+	//     }
 
-    //     console.log(JSON.stringify(res.ops, undefined, 2));
-    // });
+	//     console.log(JSON.stringify(res.ops, undefined, 2));
+	// });
 
-    db.collection('Users').insertOne({
-        name: 'paulo',
-        age: 20,
-        Location: 'PT'
-    }, function (err,res) {
-        if (err) {
-            return console.log('unable to insert Users ', err);
-        }
+	// db.collection('Users').insertOne({
+	// 	name: 'paulo',
+	// 	age: 20,
+	// 	Location: 'PT',
+	// }, function (err, res) {
+	// 	if (err) {
+	// 		return console.log('unable to insert Users ', err);
+	// 	}
 
-        console.log(JSON.stringify(res.ops, undefined, 2));
-    });
+	// 	console.log(JSON.stringify(res.ops, undefined, 2));
+	// });
 
-    db.close();
+	db.collection('Users').find({}).toArray(function (err, res) {
+		if (err) {
+			return console.log('unable to get Users ', err);
+		}
+
+		console.log(res[0]._id);
+	});
+
+	db.close();
 });
