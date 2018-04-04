4f169b03095dabdd9bd76d60fcd93508a370f1c7
paulo_roncon <proncon@gmail.com>
2018-04-04 01:50:13 +0100

    added tests to GET /todos/:id

--- mongo-todo-api/server/tests/server.test.js
+++ mongo-todo-api/server/tests/server.test.js
@@ -1,12 +1,16 @@
 const request = require('supertest');
 const expect = require('expect');
 
+const {ObjectID} = require('mongodb');
+
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
 const todos = [{
+	_id: new ObjectID(),
 	text: 'First test todo',
 }, {
+	_id: new ObjectID(),
 	text: 'Second test todo',
 }];
 
@@ -66,16 +70,16 @@ describe('POST /todos', function () {
 				if (err) {
 					return done(err);
 				}
-      });
+			});
 
-      Todo.find({}).then(function (todos) {
-        expect(todos).toExist();
-        expect(todos.length).toBe(2);
-        done();
-      }).catch(function (e) {
-        done(e);
-      });
-  });      
+		Todo.find({}).then(function (todos) {
+			expect(todos).toExist();
+			expect(todos.length).toBe(2);
+			done();
+		}).catch(function (e) {
+			done(e);
+		});
+	});     
 });
 
 describe('GET /todos', function () {
@@ -84,15 +88,58 @@ describe('GET /todos', function () {
 			.get('/todos')
 			.expect(200)
 			.expect(function (res) {
-        expect(res.body.todos.length).toBe(2);
-        done();
+				expect(res.body.todos.length).toBe(2);
+				done();
 				//expect(res.body).toBe(todos);
 			})
 			.end(function (err, res) {
 				if (err) {
 					return done(err);
-        }
-      });
+				}
+			});
+	});
+});
 
-  });
+
+describe('GET /todos/:id', function () {
+	it('should validate id', function (done) {
+
+		const safeObjectId = id => (ObjectID.isValid(id) ? new ObjectID(id) : null);
+
+		request(app)
+			.get(`/todos/${todos[0]._id.toHexString()}`)
+			.expect(200)
+			.expect(function (res) {
+
+				let id = res.body.todo._id;
+
+				const result = safeObjectId(id)
+				expect(typeof result).toBe('object');
+				//expect(res.body).toBe(todos);
+				expect (res.body.todo.text).toBe(todos[0].text);
+			})
+
+			.end(function (err, res) {
+				if (err) {
+					return done(err);
+				}
+				done();
+			});
+	});
+
+	it('should return 404 if ID not found', function (done) {
+		var hexid = new ObjectID().toHexString();
+		request(app)
+		.get(`/todos/${hexid}`)
+		.expect(404)
+		.end(done);
+	});
+
+	it('should return 404 if invalid ID', function (done) {
+		request(app)
+		.get(`/todos/aaaaaaa`)
+		.expect(404)
+		.end(done);
+	});
+	
 });
